1347f40ad210fd03e83dca47d49862f5623c56c2
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-06 21:13:33 +0200

    Add -fno-inline option to compile_to_asm.sh

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -1,6 +1,6 @@
 # $1: name of the c file to compile to assembly
 # $2 output path
-opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
+opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -fno-inline -Wno-error -finline-limit=2"
 
 if ! gcc  -I. -I./src  -Wextra -Wall -Wno-unused-parameter -Wno-unused-function -Woverlength-strings -I./src/decNumber/ -I./modules/oniguruma/src -g -pthread $opt -S -masm=intel -o "$2" "$1"; then
 	echo "error compile to asm"
